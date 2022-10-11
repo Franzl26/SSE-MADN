3358bafaddbc2a0ebb9cfa84eb83ff12a7a57f12
Frank Lucassen <frank.lucassen@unibw.de>
2022-10-11 14:29:39 +0200

    hash

--- src/main/java/App/Start.java
+++ src/main/java/App/Start.java
@@ -5,6 +5,10 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Locale;
 
 public class Start extends Application {
@@ -24,6 +28,32 @@ public class Start extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        //launch();
+        hash();
+    }
+
+    public static void hash() {
+        String password = "ILoveJava";
+
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        System.out.println(bytesToHex(digest));
+    }
+
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
     }
 }
